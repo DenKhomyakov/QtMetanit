9d080dcf97c69c562d784baea8b099bc9335f7e5
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-01-16 14:15:50 +0700

    Positioned widgets (buttons)

--- MyQtWidget/main.cpp
+++ MyQtWidget/main.cpp
@@ -1,20 +1,37 @@
 #include <QApplication>
 #include <QWidget>
-#include <Qlabel>
 #include <QVBoxLayout>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv); // Объект приложения
 
-    // Первый способ управления компоновкой
-    QWidget* widget1 = new QWidget;
-    QVBoxLayout* layout1 = new QVBoxLayout(widget1);
+    // Создаём виджет
+    QWidget* widget = new QWidget();
+    widget->setWindowTitle("My widget");
+    widget->setMinimumHeight(300);
+    widget->setMinimumWidth(300);
 
-    // Второй способ управления компоновкой
-    QWidget* widget2 = new QWidget;
-    QVBoxLayout* layout2 = new QVBoxLayout();
-    widget2->setLayout(layout2);
+    // Создаём набор вложенных кнопок
+    QPushButton* button1 = new QPushButton("Button 1");
+    QPushButton* button2 = new QPushButton("Button 2");
+    QPushButton* button3 = new QPushButton("Button 3");
+    QPushButton* button4 = new QPushButton("Button 4");
 
+    // Создаём контейнер для расположения виджетов по вертикали
+    QVBoxLayout* layout = new QVBoxLayout();
+    widget->setLayout(layout);
+
+    // Добавляем виджеты (кнопки) в контейнер
+    layout->addWidget(button1);
+    layout->addSpacing(100);
+    layout->addWidget(button2);
+    layout->addSpacing(100);
+    layout->addWidget(button3);
+    layout->addWidget(button4);
+    layout->addSpacing(50);
+
+    widget->show();
     return app.exec();
 }
