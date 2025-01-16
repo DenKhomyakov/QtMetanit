afbc5fa81c3c777661ae350fbe6325eef910f881
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-01-16 16:44:34 +0700

    Using a button press and text message

--- MyQtWidget/main.cpp
+++ MyQtWidget/main.cpp
@@ -1,34 +1,34 @@
 #include <QApplication>
 #include <QWidget>
-#include <QGridLayout>
 #include <QPushButton>
+#include <QMessageBox>
+
+void onClick();
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv); // Объект приложения
 
-    // Создаём виджет
     QWidget* widget = new QWidget();
-    widget->setWindowTitle("My widget");
+    widget->setWindowTitle("QPushButton");
     widget->setMinimumHeight(300);
     widget->setMinimumWidth(300);
 
-    // Создаём набор вложенных кнопок
-    QPushButton* button1 = new QPushButton("Button 1");
-    QPushButton* button2 = new QPushButton("Button 2");
-    QPushButton* button3 = new QPushButton("Button 3");
-    QPushButton* button4 = new QPushButton("Button 4");
+    QPushButton* button = new QPushButton("Click me", widget);
 
-    // Создаём контейнер для расположения виджетов по вертикали
-    QGridLayout* layout = new QGridLayout();
-    widget->setLayout(layout);
-
-    // Добавляем виджеты (кнопки) в контейнер
-    layout->addWidget(button1, 0, 0);
-    layout->addWidget(button2, 0, 1);
-    layout->addWidget(button3, 1, 0);
-    layout->addWidget(button4, 1, 1);
+    // Устанавливаем для события clicked обработчик onClick
+    QObject::connect(button, &QPushButton::clicked, onClick);
 
     widget->show();
     return app.exec();
+}
+
+void onClick() {
+    QMessageBox* messageBox = new QMessageBox();
+    messageBox->setWindowTitle("New window");
+    messageBox->setMinimumHeight(150);
+    messageBox->setMinimumWidth(150);
+    messageBox->setText("Hello METANIT.COM");
+    messageBox->show();
+    //messageBox->exec();
 }
